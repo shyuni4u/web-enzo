d482f5df9d4d0c9d10faea4d3acdc583e6d97590
shyuni4u <shyuniz@naver.com>
2021-07-02 16:24:11 +0900

    fix error.

---
+++ components/organisms/Study.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+import Router from 'next/router';
+
+import { toast } from 'react-toastify';
+
+import Panel from '../atoms/Panel';
+import Button from '../atoms/Button';
+
+export const Study: React.FC = () => {
+  return <>Study</>;
+};
+
+export default Study;
